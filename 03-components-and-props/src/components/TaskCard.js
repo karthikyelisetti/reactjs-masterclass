d68700d372b15cb70f07ce3fd8a9a8ecdd9391d9
karthikyelisetti <62472452+karthikyelisetti@users.noreply.github.com>
2024-03-23 15:10:05 +0530

    Adding component and props concept

---
+++ 03-components-and-props/src/components/TaskCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const TaskCard = ({ task, handleDelete }) => {
+  return (
+    <>
+      <li key={task.id} className={task.completed ? "completed" : "incomplete"}>
+        <span>
+          {task.id}-{task.name}
+        </span>
+        <button className="delete" onClick={() => handleDelete(task.id)}>
+          Delete
+        </button>
+      </li>
+    </>
+  );
+};
