0d5ed26ffaadff991a62fb4df085f8a50f769df3
karthikyelisetti <62472452+karthikyelisetti@users.noreply.github.com>
2024-05-20 07:17:50 +0530

    Adding custom hook - useFetch

--- 06-useeffect-usecallback-customhooks/src/components/ProductList.js
+++ 06-useeffect-usecallback-customhooks/src/components/ProductList.js
@@ -1,14 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
+// importing custom hook
+import { useFetch } from "../hooks/useFetch";
 
 export const ProductList = () => {
-  const [products, setProducts] = useState([]);
   const [url, setUrl] = useState(["http://localhost:8000/products"]);
+  const { data: products } = useFetch(url);
 
-  useEffect(() => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => setProducts(data));
-  }, [url]);
+  //useCallback() is needed only when the function is outside the useEffect()
+  // const fetchProducts = useCallback(async () => {
+  //   const response = await fetch(url);
+  //   const data = await response.json();
+  //   setProducts(data);
+  // }, [url])
+  
+  // useEffect(() => {
+  //   fetchProducts();
+  //   console.log("-");
+  // }, [fetchProducts]);
   
   return (
     <section>
@@ -17,7 +25,7 @@ export const ProductList = () => {
         <button onClick={() => setUrl("http://localhost:8000/products?in_stock=true")}>In Stock Only</button>
       </div>
       
-      {products.map((product) => (
+      {products && products.map((product) => (
         <div className="card" key={product.id}>
           <p className="id">{product.id}</p>
           <p className="name">{product.name}</p>
